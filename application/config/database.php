19a95fa34b6dec5ce8c23ae8f97c02847f0abb00
SHINOY P B <PBSHINOY@GMAIL.COM>
2021-12-08 22:30:19 +0530

    some fixing in database config file

--- application/config/database.php
+++ application/config/database.php
@@ -95,24 +95,4 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'localhost',
-// 	'username' => 'askmefor_askmefor',
-// 	'password' => 'pI-8Hxk2I:eN08',
-// 	'database' => 'askmefor_anymedion',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
+
